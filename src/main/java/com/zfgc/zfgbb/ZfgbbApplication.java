6a7dcfba360eadaf1b8a68eb452d1191ce3be4b8
MGZero <onisephiroth@gmail.com>
2022-02-21 12:24:32 -0500

    initial commit

---
+++ src/main/java/com/zfgc/zfgbb/ZfgbbApplication.java
@@ -0,0 +1,13 @@
+package com.zfgc.zfgbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZfgbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZfgbbApplication.class, args);
+	}
+
+}
