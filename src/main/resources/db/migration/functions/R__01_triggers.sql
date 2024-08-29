48b2ca473bf15646bd3299a499471c2fd1d90b2b
steve <onisephiroth@gmail.com>
2024-08-29 19:27:42 -0400

    starting to pull message creation together

--- src/main/resources/db/migration/functions/R__01_triggers.sql
+++ src/main/resources/db/migration/functions/R__01_triggers.sql
@@ -1,19 +1,22 @@
 create or replace function zfgbb.set_current_msg_after_insert()
 returns trigger as $$
 begin
-	update zfgbb.message_history mh
-	set mh.current_flag = 1
-	where mh.message_history_id = new.message_history_id;
+	update zfgbb.message_history
+	set current_flag = true
+	where message_history_id = new.message_history_id;
 
-	update zfgbb.message_history mh
-	set mh.current_flag = 0
-	where mh.message_history_id <> new.message_history_id and
-	      mh.message_id = new.message_id;
+	update zfgbb.message_history
+	set current_flag = false
+	where message_history_id <> new.message_history_id and
+	      message_id = new.message_id;
+	      
+	return new;
 end;
 $$ language plpgsql;
 
-drop trigger if exists message_history_after_insert on message_history_after_insert;
-create trigger message_history_after_insert
+drop trigger if exists trigger_message_history_after_insert on zfgbb.message_history;
+create trigger trigger_message_history_after_insert
 after insert on zfgbb.message_history
-execute procedure zfgbb.set_current_msg_after_insert();
+for each row execute procedure zfgbb.set_current_msg_after_insert();
 
+--drop trigger if exists trigger_message_updated_ts on zfgbb.message;
