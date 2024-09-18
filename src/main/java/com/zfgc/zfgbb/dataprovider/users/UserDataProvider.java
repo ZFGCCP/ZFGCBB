ecc6108e5095afa7f436eeb93bc890ba921ea233
steve <onisephiroth@gmail.com>
2024-09-18 19:00:04 -0400

    add permissions routine

--- src/main/java/com/zfgc/zfgbb/dataprovider/users/UserDataProvider.java
+++ src/main/java/com/zfgc/zfgbb/dataprovider/users/UserDataProvider.java
@@ -17,6 +17,7 @@ import com.zfgc.zfgbb.config.loadoption.user.LoggedInUserLoadOptions;
 import com.zfgc.zfgbb.dao.UserPermissionViewDao;
 import com.zfgc.zfgbb.dataprovider.AbstractDataProvider;
 import com.zfgc.zfgbb.dbo.EmailAddressDbo;
+import com.zfgc.zfgbb.dbo.UserBioInfoDbo;
 import com.zfgc.zfgbb.dbo.UserDbo;
 import com.zfgc.zfgbb.dbo.UserDboExample;
 import com.zfgc.zfgbb.dbo.UserPermissionViewDboExample;
@@ -40,58 +41,12 @@ public class UserDataProvider extends AbstractDataProvider {
 	@Autowired
 	private UserBioInfoDao bioInfoDao;
 	
-	@Autowired
-	private AvatarDao avatarDao;
-	
 	public User getUser(String userName) {
 		UserDboExample ex = new UserDboExample();
 		ex.createCriteria().andUserNameEqualTo(userName).andActiveFlagEqualTo(true);
 		UserDbo userDb = userDao.get(ex).stream().findFirst().orElse(createGuest());
 
-		return getUser(userDb.getPkId(), new LoggedInUserLoadOptions());
-	}
-	
-	public User getUser(Integer userId, BasicUserLoadOptions loadOptions) {
-		if(loadOptions == null) {
-			loadOptions = new BasicUserLoadOptions();
-		}
-		
-		UserDboExample ex = new UserDboExample();
-		ex.createCriteria().andUserIdEqualTo(userId).andActiveFlagEqualTo(true);
-		UserDbo userDb = userDao.get(ex).stream().findFirst().orElse(createGuest());
-		
-		User user = mapper.map(userDb, User.class);
-
-		//todo: setup guest permissions
-		if(Boolean.TRUE.equals(loadOptions.loadPermissions())){
-			UserPermissionViewDboExample upEx = new UserPermissionViewDboExample();
-			upEx.createCriteria().andUserIdEqualTo(user.getUserId());
-			List<Permission> permissions = convertDboListToModel(userPermissionDao.get(upEx), Permission.class);
-	
-			user.setPermissions(permissions);
-		}
-		
-		if(Boolean.TRUE.equals(loadOptions.loadBio())) {
-			UserBioInfoDbo bioInfoDbo = bioInfoDao.get(userId);
-			if(bioInfoDbo != null) {
-				user.setBioInfo(mapper.map(bioInfoDbo, UserBioInfo.class));
-			}
-		}
-		
-		if(Boolean.TRUE.equals(loadOptions.loadAvatar())) {
-			AvatarDboExample avatarEx = new AvatarDboExample();
-			avatarEx.createCriteria().andUserIdEqualTo(userId)
-									 .andActiveFlagEqualTo(true);
-			
-			Optional<AvatarDbo> avDb = avatarDao.get(avatarEx).stream().findFirst();
-			
-			if(avDb != null) {
-				Avatar av = avDb.map((a) -> mapper.map(a, Avatar.class)).orElse(null);
-				user.setAvatar(av);
-			}
-		}
-		
-		return user;
+		return getUser(userDb.getPkId());
 	}
 	
 	public User getUser(Integer userId) {
@@ -108,12 +63,20 @@ public class UserDataProvider extends AbstractDataProvider {
 
 		user.setPermissions(permissions);
 		
+		UserBioInfoDbo bioInfoDbo = bioInfoDao.get(userId);
+		user.setBioInfo(mapper.map(bioInfoDbo, UserBioInfo.class));
+		
 		return user;
 	}
 	
 	public User createUser(User user) {
 		UserDbo userDbo = mapper.map(user, UserDbo.class);
 		userDao.save(userDbo);
+		
+		//create bio info
+		UserBioInfoDbo bioInfo = mapper.map(user.getBioInfo(), UserBioInfoDbo.class);
+		bioInfo.setUserId(userDbo.getPkId());
+		bioInfoDao.save(bioInfo);
 		
 		EmailAddressDbo emailDbo = mapper.map(user.getEmail(), EmailAddressDbo.class);
 		emailDbo.setUserId(userDbo.getPkId());
