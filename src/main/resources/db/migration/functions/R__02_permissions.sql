9bafafd63c21fcf4d0f1334c9cda630bdd2684f0
steve <onisephiroth@gmail.com>
2024-09-30 23:38:36 -0400

    add user permission tables and a couple of other table updates

--- src/main/resources/db/migration/functions/R__02_permissions.sql
+++ src/main/resources/db/migration/functions/R__02_permissions.sql
@@ -13,6 +13,38 @@ begin
 	
 end; $$;
 
+create or replace function zfgbb.create_permission_group(p_group_id int,
+														 p_group_name text, 
+														 p_description text, 
+														 p_min_posts int,
+														 p_parent_group int)
+returns void
+language plpgsql
+as $$
+begin
+	
+	insert into zfgbb.permission_group(permission_group_id, group_name, description, min_posts, star_image, parent_group)
+	values(p_group_id, p_group_name, p_description, -1, 'default.png', p_parent_group)
+	on conflict(permission_group_id)
+	do update set group_name = p_group_name, description = p_description, min_posts = p_min_posts, star_image = 'default.png', parent_group = p_parent_group;
+	
+	return;
+	
+end; $$;
+
+create or replace function zfgbb.associate_permission_to_group(p_group_name text, p_permission_name text)
+returns void
+language plpgsql
+as $$
+begin
+	
+	insert into zfgbb.permission_group_assoc(permission_group_id, permission_id)
+	values((select permission_group_id from zfgbb.permission_group where group_name = p_group_name),
+		   (select permission_id from zfgbb.permission where permission_code = p_permission_name)
+		   );
+	
+	return;
+end; $$;
 
 select zfgbb.create_permission(1, 'ZFGC User', 'ZFGC_USER');
 select zfgbb.create_permission(2, 'ZFGC Guest', 'ZFGC_GUEST');
@@ -20,3 +52,7 @@ select zfgbb.create_permission(3, 'User Profile Viewer', 'ZFGC_PROFILE_VIEWER');
 select zfgbb.create_permission(4, 'User Profile Editor', 'ZFGC_PROFILE_EDITOR');
 select zfgbb.create_permission(5, 'User Profile Admin', 'ZFGC_PROFILE_ADMIN');
 
+select zfgbb.create_permission_group(1, 'Member', '', -1, null);
+select zfgbb.create_permission_group(2, 'Admin', '', -1, 1);
+
+select zfgbb.associate_permission_to_group('Member', 'ZFGC_USER');
