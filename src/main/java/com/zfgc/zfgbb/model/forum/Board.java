a35776fc914e1743898bc21fca58a1c902adb51b
MGZero <onisephiroth@gmail.com>
2024-10-14 15:13:15 -0400

    refactors forum endpoints a bit (#26)

--- src/main/java/com/zfgc/zfgbb/model/forum/Board.java
+++ src/main/java/com/zfgc/zfgbb/model/forum/Board.java
@@ -1,6 +1,5 @@
 package com.zfgc.zfgbb.model.forum;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -20,9 +19,6 @@ public class Board extends BaseModel {
 	private List<Thread> stickyThreads;
 	private List<Thread> unStickyThreads;
 	private List<BoardSummary> childBoards;
-	
-	@JsonIgnore
-	private List<Permission> boardPerms = new ArrayList<>();
 	
 	public List<Thread> getStickyThreads() {
 		return stickyThreads;
@@ -111,14 +107,6 @@ public class Board extends BaseModel {
 			return 1L;
 		}
 		return (long) Math.ceil(threadCount.doubleValue() / 10.0);
-	}
-
-	public List<Permission> getBoardPerms() {
-		return boardPerms;
-	}
-
-	public void setBoardPerms(List<Permission> boardPerms) {
-		this.boardPerms = boardPerms;
 	}
 
 
