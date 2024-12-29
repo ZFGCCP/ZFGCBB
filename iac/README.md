02c2230a03061fc617d39c7b8627e1a2dc736e5b
Jonathan Basniak <jon@bluedreamers.com>
2024-12-29 13:48:33 -0500

    feat(iac): add k8s project

---
+++ iac/README.md
@@ -0,0 +1,25 @@
+# Infrastructure as Code
+
+This directory contains the infrastructure as code for the ZFGCBB project.
+
+## Table of Contents
+
+- [Infrastructure as Code](#infrastructure-as-code)
+  - [Table of Contents](#table-of-contents)
+  - [Requirements](#requirements)
+  - [Getting Started](#getting-started)
+  - [License](#license)
+
+## Requirements
+
+- [Docker](https://docs.docker.com/get-docker/)
+- [Kubectl](https://kubernetes.io/docs/tasks/tools/)
+- [Kustomize](https://kustomize.io/)
+
+## Getting Started
+
+For now, just look at [](zfgc.com/kube/README.md)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](../LICENSE) file for details.
---
+++ iac/zfgc.com/scripts/bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install apt-transport-https curl -y
+
+sudo apt install containerd -y
+
+
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
+sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
