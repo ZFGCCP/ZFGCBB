6013fd7718c0a60885e1f57137f5551146af525d
Jonathan Basniak <jon@bluedreamers.com>
2025-01-14 22:07:29 -0500

    fix: make dev setup scripts consistent

--- scripts/development/setup/setup-linux.sh
+++ scripts/development/setup/setup-linux.sh
@@ -1,44 +1,87 @@
 #!/bin/bash
 set -e
 
-# Check distro
-if [ "$(lsb_release -si)" == "Ubuntu" ]; then
-    # Check if we're running as root
-    if [ "$EUID" -ne 0 ]; then
-        echo "Please run this script as root."
-        exit 1
-    fi
+INSTALL_IAC_STUFF=false
+INSTALL_JAVA_STUFF=false
 
+# Ask if we want to install IAC stuff
+echo "Do you want to install Docker, Docker Compose, and Minikube? (y/n)"
+read -r answer
+if [ "$answer" == "y" ]; then
+    INSTALL_IAC_STUFF=true
+fi
+
+# Ask if we want to install Java stuff
+echo "Do you want to install Java JDK 17? (y/n)"
+read -r answer
+if [ "$answer" == "y" ]; then
+    INSTALL_JAVA_STUFF=true
+fi
+
+install_ubuntu() {
     sudo apt-get update
     # Install dependencies
-    # Install Docker, Docker Compose, and Minikube
-    sudo apt-get install -y docker.io docker-compose minikube openjdk-17-jdk
+    if [ "$INSTALL_IAC_STUFF" = true ]; then
+        # Install Docker, Docker Compose, and Minikube
+        sudo apt-get install -y docker.io docker-compose minikube
+    fi
 
-    echo "Docker installed successfully."
-# Else if we're running on Arch Linux
-elif [ "$(lsb_release -si)" == "Arch" ]; then
+    if [ "$INSTALL_JAVA_STUFF" = true ]; then
+        # Install Java JDK 17
+        sudo apt-get install -y openjdk-17-jdk
+    fi
+}
+
+install_arch() {
     sudo pacman -Syu
-    sudo pacman -S docker docker-compose minikube openjdk17-jdk
-else 
+    if [ "$INSTALL_IAC_STUFF" = true ]; then
+        # Install Docker, Docker Compose, and Minikube
+        sudo pacman -S docker docker-compose minikube
+    fi
+
+    if [ "$INSTALL_JAVA_STUFF" = true ]; then
+        # Install Java JDK 17
+        sudo pacman -S openjdk17-jdk
+    fi
+}
+
+install_unknown() {
+    echo "We don't know your distro. So, we'll just check the presence of the dependencies."
+
     # Just check if there are missing dependencies, then list them out
-    if ! command -v docker &> /dev/null; then
-        echo "Docker is not installed. Please install it first."
-    fi
-    if ! command -v kubectl &> /dev/null; then
-        echo "Kubernetes CLI is not installed. Please install it first."
-    fi
-    if ! command -v minikube &> /dev/null; then
-        echo "Minikube is not installed. Please install it first."
-    fi
-    if ! command -v java &> /dev/null; then
-        echo "Java JDK 17 is not installed. Please install it first."
-    else
-        java_version=$(java -version 2>&1 | head -n 1 | awk -F '"' '{print $2}')
-        if [ "$java_version" != "17" ]; then
-            echo "Java JDK 17 is not installed. Please install it first."
+    if [ "$INSTALL_IAC_STUFF" = true ]; then
+        if ! command -v docker &> /dev/null; then
+            echo "Docker is not installed. Please install it first."
+        fi
+        if ! command -v kubectl &> /dev/null; then
+            echo "Kubernetes CLI is not installed. Please install it first."
+        fi
+        if ! command -v minikube &> /dev/null; then
+            echo "Minikube is not installed. Please install it first."
         fi
     fi
+    
+    if [ "$INSTALL_JAVA_STUFF" = true ]; then
+        if ! command -v java &> /dev/null; then
+            echo "Java JDK 17 is not installed. Please install it first."
+        else
+            java_version=$(java -version 2>&1 | head -n 1 | awk -F '"' '{print $2}')
+            if [ "$java_version" != "17" ]; then
+                echo "Java JDK 17 is not installed. Please install it first."
+            fi
+        fi
+    fi
+
     exit 0
+}
+
+# Check distro
+if [ "$(lsb_release -si)" == "Ubuntu" ]; then
+    install_ubuntu
+# Else if we're running on Arch Linux
+elif [ "$(lsb_release -si)" == "Arch" ]; then
+    install_arch
+else 
 fi
 
 echo "Starting Docker..."
