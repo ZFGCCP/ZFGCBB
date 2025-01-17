8134c32adf62d7ccdaa5435a47ad2bcc2e07fa7f
Jonathan Basniak <jon@bluedreamers.com>
2025-01-17 08:49:55 -0500

    chore: add mg-config-zero

--- scripts/development/setup/setup-windows.sh
+++ scripts/development/setup/setup-windows.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 set -e
 
-# Check if winget is installed
 if ! command -v winget &> /dev/null; then
     echo "winget is not installed. Please install it first."
     exit 1
 fi
 
-# Check if we're running as admin
 if [ "$(whoami)" != "Administrator" ]; then
     echo "Please run this script as an administrator."
     exit 1
 fi
 
-# Ask if we want to install WSL
 wsl --list --quiet > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "WSL is not installed. Do you want to install it now? (y/n)"
@@ -37,7 +34,6 @@ else
     echo "Skipping installation of Docker Desktop, Kubernetes CLI, and Minikube."
 fi
 
-# Ask if we want to install Java JDK 17
 java_version=$(java -version 2>&1 | head -n 1 | awk -F '"' '{print $2}')
 if [ "$java_version" != "17" ]; then
     echo "Java JDK 17 is not installed. Do you want to install it now? (y/n)"
@@ -50,3 +46,15 @@ if [ "$java_version" != "17" ]; then
     fi
 fi
 
+# Ask if we want to install mvn, first check if mvn is installed
+mvn --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "mvn is not installed. Do you want to install it now? (y/n)"
+    read -r answer
+    if [ "$answer" == "y" ]; then
+        echo "Installing mvn..."
+        winget install -e --id Apache.Maven
+    else
+        echo "Skipping installation of mvn."
+    fi
+fi
