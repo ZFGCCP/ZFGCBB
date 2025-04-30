63e7c1d4bab64014a7bf17b2c08015cd516ed0b0
Jonathan Basniak <jon@bluedreamers.com>
2025-04-30 17:09:07 -0400

    chore: fix usage help

--- scripts/development/deployment/bootstrap-server-for-deployment-automation.sh
+++ scripts/development/deployment/bootstrap-server-for-deployment-automation.sh
@@ -14,14 +14,14 @@ usage() {
   echo ""
   echo "  -h HOST_NAME       Hostname or IP address (e.g., 192.0.2.10)"
   echo "  -u USER            SSH username (e.g., root)"
-  echo " -p PORT             Port to use (e.g., 8080)"
+  echo "  -p PORT             Port to use (e.g., 8080)"
   echo ""
   echo "Example:"
   echo "  $0 -h 192.0.2.10 -u root"
   exit 1
 }
 
-while getopts ":h:u:" opt; do
+while getopts ":h:u:p:" opt; do
   case $opt in
     h) HOST_NAME="$OPTARG" ;;
     u) USER_NAME="$OPTARG" ;;
